fd6650d42e454cd4f8543c7487e42e9ce235d255
taha <taha@gmail.com>
2024-04-23 14:09:36 -0700

    up

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,7 +2,7 @@
 import os
 from fabric.api import *
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['100.25.140.50', '35.168.1.104']
 
 
 def do_clean(number=0):
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['100.25.140.50', '35.168.1.104']
 
 
 def do_pack():
